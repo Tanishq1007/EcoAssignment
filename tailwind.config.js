a4c1e48f355d811dc8676caa45122bf662ce46e4
Tanishq1007 <132736717+Tanishq1007@users.noreply.github.com>
2024-10-09 00:36:13 +0530

    creation

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}', // Ensure it scans your components for classes
+  ],
+  theme: {
+    extend: {
+      // Extend any custom styles if needed
+    },
+  },
+  plugins: [],
+};
